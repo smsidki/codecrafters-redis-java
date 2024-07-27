b44e022e858024cff07b683f2343f686f832409c
sayid.sidqi <sayid.sidqi@accenture.com>
2024-07-28 00:01:53 +0700

    respond to multiple PINGs: handle connection reset

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,43 +37,58 @@ public class Main {
       }));
 
       while (running[0]) {
-        if (selector.select(5000) == 0) {
-          continue;
-        }
-
-        var keys = selector.selectedKeys();
-        var iterator = keys.iterator();
-        while (iterator.hasNext()) {
-          var key = iterator.next();
+        selector.select(5000);
+        for (var key : selector.selectedKeys()) {
           if (key.isAcceptable()) {
             var clientCh = serverCh.accept();
-            clientCh.configureBlocking(false);
-            clientCh.register(selector, SelectionKey.OP_READ);
-            System.out.println("Client connected! " + clientCh.getRemoteAddress());
+            if (clientCh != null) {
+              clientCh.configureBlocking(false);
+              clientCh.register(selector, SelectionKey.OP_READ);
+              System.out.println("Client connected! " + clientCh.getRemoteAddress());
+            }
           } else if (key.isReadable()) {
-            var clientCh = (SocketChannel) key.channel();
-            var buffer = ByteBuffer.allocate(30);
-            var bytesRead = clientCh.read(buffer);
-            if (bytesRead == -1) {
-              var clientAddr = clientCh.getRemoteAddress();
-              clientCh.close();
-              System.out.println("Client disconnected! " + clientAddr);
-            } else {
-              buffer.flip();
-              var cmd = StandardCharsets.UTF_8.decode(buffer).toString().trim();
-              buffer.clear();
-              System.out.println("Command: " + cmd);
+            try {
+              var clientCh = (SocketChannel) key.channel();
+              var buffer = ByteBuffer.allocate(30);
+              var bytesRead = clientCh.read(buffer);
+              if (bytesRead == -1) {
+                var clientAddr = clientCh.getRemoteAddress();
+                clientCh.close();
+                System.out.println("Client disconnected! " + clientAddr);
+              } else {
+                buffer.flip();
+                var cmd = StandardCharsets.UTF_8.decode(buffer).toString().trim();
+                buffer.clear();
+                System.out.println("Command: " + cmd);
 
-              buffer.put("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
-              buffer.flip();
-              clientCh.write(buffer);
+                buffer.put("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                  clientCh.write(buffer);
+                }
+              }
+            } catch (Exception e) {
+              System.err.println("Fail processing request: " + e.getMessage());
+              //noinspection CallToPrintStackTrace
+              e.printStackTrace();
+              //noinspection resource
+              if (key.channel() != null) {
+                try {
+                  key.channel().close();
+                } catch (Exception ex) {
+                  System.err.println("Fail closing channel: " + ex.getMessage());
+                  //noinspection CallToPrintStackTrace
+                  ex.printStackTrace();
+                }
+              }
             }
           }
         }
-        iterator.remove();
       }
     } catch (IOException e) {
       System.err.println("Failed to open selector: " + e.getMessage());
+      //noinspection CallToPrintStackTrace
+      e.printStackTrace();
     }
   }
 
